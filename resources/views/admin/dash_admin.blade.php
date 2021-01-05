6581dde15f209cff5e874ab6243a789d3a7e33d3
Rizki Dwi N <72845868+RizkiDwiN@users.noreply.github.com>
2021-01-06 00:15:08 +0700

    Add Fitures Dash

--- resources/views/admin/dash_admin.blade.php
+++ resources/views/admin/dash_admin.blade.php
@@ -129,23 +129,11 @@
         <div class="card-header card-header-tabs card-header-primary">
           <div class="nav-tabs-navigation">
             <div class="nav-tabs-wrapper">
-              <span class="nav-tabs-title">Tasks:</span>
+              <span class="nav-tabs-title">Notification:</span>
               <ul class="nav nav-tabs" data-tabs="tabs">
                 <li class="nav-item">
                   <a class="nav-link active" href="#profile" data-toggle="tab">
-                    <i class="material-icons">bug_report</i> Bugs
-                    <div class="ripple-container"></div>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#messages" data-toggle="tab">
-                    <i class="material-icons">code</i> Website
-                    <div class="ripple-container"></div>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#settings" data-toggle="tab">
-                    <i class="material-icons">cloud</i> Server
+                    <i class="material-icons">notifications</i> Konfirmation
                     <div class="ripple-container"></div>
                   </a>
                 </li>
@@ -160,206 +148,39 @@
                 <tbody>
                   <tr>
                     <td>
-                      <div class="form-check">
-                        <label class="form-check-label">
-                          <input class="form-check-input" type="checkbox" value="" checked>
-                          <span class="form-check-sign">
-                            <span class="check"></span>
-                          </span>
-                        </label>
-                      </div>
+                      <b>Warung Name</b>
                     </td>
-                    <td>Sign contract for "What are conference organizers afraid of?"</td>
+                    <td>You have a new Warung, Check detail to confirm.</td>
                     <td class="td-actions text-right">
-                      <button type="button" rel="tooltip" title="Edit Task" class="btn btn-primary btn-link btn-sm">
-                        <i class="material-icons">edit</i>
-                      </button>
-                      <button type="button" rel="tooltip" title="Remove" class="btn btn-danger btn-link btn-sm">
-                        <i class="material-icons">close</i>
-                      </button>
+                      <a href="#" class="btn btn-primary">Details</a>
                     </td>
                   </tr>
                   <tr>
                     <td>
-                      <div class="form-check">
-                        <label class="form-check-label">
-                          <input class="form-check-input" type="checkbox" value="">
-                          <span class="form-check-sign">
-                            <span class="check"></span>
-                          </span>
-                        </label>
-                      </div>
+                    <b>Warung Name</b>
                     </td>
-                    <td>Lines From Great Russian Literature? Or E-mails From My Boss?</td>
+                    <td>You have a new Warung, Check detail to confirm.</td>
                     <td class="td-actions text-right">
-                      <button type="button" rel="tooltip" title="Edit Task" class="btn btn-primary btn-link btn-sm">
-                        <i class="material-icons">edit</i>
-                      </button>
-                      <button type="button" rel="tooltip" title="Remove" class="btn btn-danger btn-link btn-sm">
-                        <i class="material-icons">close</i>
-                      </button>
+                      <a href="#" class="btn btn-primary">Details</a>
                     </td>
                   </tr>
                   <tr>
                     <td>
-                      <div class="form-check">
-                        <label class="form-check-label">
-                          <input class="form-check-input" type="checkbox" value="">
-                          <span class="form-check-sign">
-                            <span class="check"></span>
-                          </span>
-                        </label>
-                      </div>
+                    <b>Warung Name</b>
                     </td>
-                    <td>Flooded: One year later, assessing what was lost and what was found when a ravaging rain swept through metro Detroit
+                    <td>You have a new Warung, Check detail to confirm.
                     </td>
                     <td class="td-actions text-right">
-                      <button type="button" rel="tooltip" title="Edit Task" class="btn btn-primary btn-link btn-sm">
-                        <i class="material-icons">edit</i>
-                      </button>
-                      <button type="button" rel="tooltip" title="Remove" class="btn btn-danger btn-link btn-sm">
-                        <i class="material-icons">close</i>
-                      </button>
+                      <a href="#" class="btn btn-primary">Details</a>
                     </td>
                   </tr>
                   <tr>
                     <td>
-                      <div class="form-check">
-                        <label class="form-check-label">
-                          <input class="form-check-input" type="checkbox" value="" checked>
-                          <span class="form-check-sign">
-                            <span class="check"></span>
-                          </span>
-                        </label>
-                      </div>
+                    <b>Warung Name</b>
                     </td>
-                    <td>Create 4 Invisible User Experiences you Never Knew About</td>
+                    <td>You have a new Warung, Check detail to confirm.</td>
                     <td class="td-actions text-right">
-                      <button type="button" rel="tooltip" title="Edit Task" class="btn btn-primary btn-link btn-sm">
-                        <i class="material-icons">edit</i>
-                      </button>
-                      <button type="button" rel="tooltip" title="Remove" class="btn btn-danger btn-link btn-sm">
-                        <i class="material-icons">close</i>
-                      </button>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-            <div class="tab-pane" id="messages">
-              <table class="table">
-                <tbody>
-                  <tr>
-                    <td>
-                      <div class="form-check">
-                        <label class="form-check-label">
-                          <input class="form-check-input" type="checkbox" value="" checked>
-                          <span class="form-check-sign">
-                            <span class="check"></span>
-                          </span>
-                        </label>
-                      </div>
-                    </td>
-                    <td>Flooded: One year later, assessing what was lost and what was found when a ravaging rain swept through metro Detroit
-                    </td>
-                    <td class="td-actions text-right">
-                      <button type="button" rel="tooltip" title="Edit Task" class="btn btn-primary btn-link btn-sm">
-                        <i class="material-icons">edit</i>
-                      </button>
-                      <button type="button" rel="tooltip" title="Remove" class="btn btn-danger btn-link btn-sm">
-                        <i class="material-icons">close</i>
-                      </button>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <div class="form-check">
-                        <label class="form-check-label">
-                          <input class="form-check-input" type="checkbox" value="">
-                          <span class="form-check-sign">
-                            <span class="check"></span>
-                          </span>
-                        </label>
-                      </div>
-                    </td>
-                    <td>Sign contract for "What are conference organizers afraid of?"</td>
-                    <td class="td-actions text-right">
-                      <button type="button" rel="tooltip" title="Edit Task" class="btn btn-primary btn-link btn-sm">
-                        <i class="material-icons">edit</i>
-                      </button>
-                      <button type="button" rel="tooltip" title="Remove" class="btn btn-danger btn-link btn-sm">
-                        <i class="material-icons">close</i>
-                      </button>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-            <div class="tab-pane" id="settings">
-              <table class="table">
-                <tbody>
-                  <tr>
-                    <td>
-                      <div class="form-check">
-                        <label class="form-check-label">
-                          <input class="form-check-input" type="checkbox" value="">
-                          <span class="form-check-sign">
-                            <span class="check"></span>
-                          </span>
-                        </label>
-                      </div>
-                    </td>
-                    <td>Lines From Great Russian Literature? Or E-mails From My Boss?</td>
-                    <td class="td-actions text-right">
-                      <button type="button" rel="tooltip" title="Edit Task" class="btn btn-primary btn-link btn-sm">
-                        <i class="material-icons">edit</i>
-                      </button>
-                      <button type="button" rel="tooltip" title="Remove" class="btn btn-danger btn-link btn-sm">
-                        <i class="material-icons">close</i>
-                      </button>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <div class="form-check">
-                        <label class="form-check-label">
-                          <input class="form-check-input" type="checkbox" value="" checked>
-                          <span class="form-check-sign">
-                            <span class="check"></span>
-                          </span>
-                        </label>
-                      </div>
-                    </td>
-                    <td>Flooded: One year later, assessing what was lost and what was found when a ravaging rain swept through metro Detroit
-                    </td>
-                    <td class="td-actions text-right">
-                      <button type="button" rel="tooltip" title="Edit Task" class="btn btn-primary btn-link btn-sm">
-                        <i class="material-icons">edit</i>
-                      </button>
-                      <button type="button" rel="tooltip" title="Remove" class="btn btn-danger btn-link btn-sm">
-                        <i class="material-icons">close</i>
-                      </button>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <div class="form-check">
-                        <label class="form-check-label">
-                          <input class="form-check-input" type="checkbox" value="" checked>
-                          <span class="form-check-sign">
-                            <span class="check"></span>
-                          </span>
-                        </label>
-                      </div>
-                    </td>
-                    <td>Sign contract for "What are conference organizers afraid of?"</td>
-                    <td class="td-actions text-right">
-                      <button type="button" rel="tooltip" title="Edit Task" class="btn btn-primary btn-link btn-sm">
-                        <i class="material-icons">edit</i>
-                      </button>
-                      <button type="button" rel="tooltip" title="Remove" class="btn btn-danger btn-link btn-sm">
-                        <i class="material-icons">close</i>
-                      </button>
+                      <a href="#" class="btn btn-primary">Details</a>
                     </td>
                   </tr>
                 </tbody>
@@ -372,41 +193,46 @@
     <div class="col-lg-6 col-md-12">
       <div class="card">
         <div class="card-header card-header-warning">
-          <h4 class="card-title">Employees Stats</h4>
-          <p class="card-category">New employees on 15th September, 2016</p>
+          <h4 class="card-title">Warung Stats</h4>
+          <p class="card-category">New Warung on 06th January, 2021</p>
         </div>
         <div class="card-body table-responsive">
           <table class="table table-hover">
             <thead class="text-warning">
               <th>ID</th>
               <th>Name</th>
-              <th>Salary</th>
-              <th>Country</th>
+              <th>City</th>
+              <th>Districts</th>
+              <th>Accepted</th>
             </thead>
             <tbody>
               <tr>
                 <td>1</td>
                 <td>Dakota Rice</td>
-                <td>$36,738</td>
-                <td>Niger</td>
+                <td>Bandung</td>
+                <td>Coblong</td>
+                <td>19:01 WIB</td>
               </tr>
               <tr>
                 <td>2</td>
-                <td>Minerva Hooper</td>
-                <td>$23,789</td>
-                <td>Curaçao</td>
+                <td>Warung Miss Claude</td>
+                <td>Bekasi</td>
+                <td>East Bekasi</td>
+                <td>18:49 WIB</td>
               </tr>
               <tr>
                 <td>3</td>
-                <td>Sage Rodriguez</td>
-                <td>$56,142</td>
-                <td>Netherlands</td>
+                <td>Warung Mr Beddy</td>
+                <td>Bogor</td>
+                <td>Hotwater</td>
+                <td>17:19 WIB</td>
               </tr>
               <tr>
                 <td>4</td>
-                <td>Philip Chaney</td>
-                <td>$38,735</td>
-                <td>Korea, South</td>
+                <td>Warung Indomie 99</td>
+                <td>Bandung</td>
+                <td>West Java</td>
+                <td>16:44 WIB</td>
               </tr>
             </tbody>
           </table>
