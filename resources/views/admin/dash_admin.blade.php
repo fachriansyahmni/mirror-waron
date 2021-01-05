0206ae00611f28e60aef60484d7e32a351fe4321
fachriansyahmni <fachriansyahmni@gmail.com>
2021-01-05 21:37:40 +0700

    fix login, add logout action

--- resources/views/admin/dash_admin.blade.php
+++ resources/views/admin/dash_admin.blade.php
@@ -148,8 +148,11 @@
                   <a class="dropdown-item" href="#">Profile</a>
                   <a class="dropdown-item" href="#">Settings</a>
                   <div class="dropdown-divider"></div>
-                  <a class="dropdown-item" href="#">Log out</a>
+                  <a class="dropdown-item" href="{{ route('logout') }}"onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Log out</a>
                 </div>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                  @csrf
+                </form>
               </li>
             </ul>
           </div>
