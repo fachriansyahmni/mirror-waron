61f7bed43032596ac27b6a936612a0752c29dd35
FermiNA29 <58778153+FermiNA29@users.noreply.github.com>
2021-01-28 22:07:34 +0700

    add total produk

--- resources/views/warung/view.blade.php
+++ resources/views/warung/view.blade.php
@@ -85,7 +85,7 @@
                         Total Produk
                 </div>
                 <div class="c-card-body text-center">
-                    8
+                    {{count($barangs)}}
                 </div>
             </div>
         </div>
