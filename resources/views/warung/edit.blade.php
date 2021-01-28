76e6489caf5453cd76021ce9ed02b760fd8963b8
fachriansyahmni <fachriansyahmni@gmail.com>
2021-01-28 22:01:12 +0700

    fix design in warung page

--- resources/views/warung/edit.blade.php
+++ resources/views/warung/edit.blade.php
@@ -1,74 +1,82 @@
 @extends('dashboard-layouts.d_master')
 
 @section('content')
-<form method="POST" enctype="multipart/form-data">
-    @csrf
-    <table>
-        <tr>
-            <td>Nama Warung : </td>
-            <td><input type="text" name="nama_warung" value="{{old('nama_warung',$DataWarung->nama_warung)}}" placeholder="Masukan Nama Warung"></td>
-        </tr>
-        <tr>
-            <td>Alamat : </td>
-            <td><input type="text" name="alamat" value="{{old('alamat',$DataWarung->alamat)}}" placeholder="Masukan Alamat Warung"></td>
-        </tr>
-        
-        <tr>
-            <td>Provinsi : </td>
-            <td>
-                @php
-                    $urlDataProvinsi = "https://dev.farizdotid.com/api/daerahindonesia/provinsi"; //ambil semua data provinsi
-                    $getDataProvinsi = json_decode(file_get_contents($urlDataProvinsi), true);
-                    // dd($getDataProvinsi);
-                @endphp
-                <select name="prov">
-                    @foreach ($getDataProvinsi["provinsi"] as $index => $provinsi)
-                        <option value="{{$provinsi["id"]}}">{{$provinsi['nama']}}</option>
-                    @endforeach
-                </select>
-            </td>
-        </tr>
-        <tr>
-            <td>Kab/Kota : </td>
-            <td id="inputKabkot">
-                <select name="kabkot">
-                </select>
-            </td>
-        </tr>
-        <tr>
-            <td>Kecamatan : </td>
-            <td>
-                <select name="kec">
-                </select>
-            </td>
-        </tr>
-        <tr>
-            <td>Nomor Handphone : </td>
-            <td><input type="text" name="no_hp" value="{{old('no_hp',$DataWarung->no_hp)}}"></td>
-        </tr>
-        <tr>
-            <td>Koordinat : </td>
-            <td><input type="text" name="koordinat" value="{{old('koordinat',$DataWarung->koordinat)}}"></td>
-        </tr>
-        <tr>
-            <td>Jenis Warung : </td>
-            <td>
-                <select name="jenis" id="">
-                    <option value="Warung Sembako">Warung Sembako</option>
-                    <option value="Warung Kelontong">Warung Kelontong</option>
-                </select>
-            </td>
-        </tr>
-        <tr>
-            <td>Foto Warung : </td>
-            <td><input type="file" name="foto"></td>
-        </tr>
-    </table>
-    <button type="submit" name="submitedit">simpan & update</button>
-</form> 
+<div class="card">
+    <div class="card-body">
+
+        <form method="POST" enctype="multipart/form-data">
+            @csrf
+            <table>
+                <tr>
+                    <td>Nama Warung : </td>
+                    <td><input type="text" class="form-control" name="nama_warung" value="{{old('nama_warung',$DataWarung->nama_warung)}}" placeholder="Masukan Nama Warung"></td>
+                </tr>
+                <tr>
+                    <td>Alamat : </td>
+                    <td><textarea name="alamat" id="" cols="30" rows="10" class="form-control">{{old('alamat',$DataWarung->alamat)}}</textarea></td>
+                </tr>
+                
+                <tr>
+                    <td>Provinsi : </td>
+                    <td>
+                        @php
+                            $urlDataProvinsi = "https://dev.farizdotid.com/api/daerahindonesia/provinsi"; //ambil semua data provinsi
+                            $getDataProvinsi = json_decode(file_get_contents($urlDataProvinsi), true);
+                            // dd($getDataProvinsi);
+                        @endphp
+                        <select name="prov" class='form-control'>
+                            @foreach ($getDataProvinsi["provinsi"] as $index => $provinsi)
+                                <option value="{{$provinsi["id"]}}">{{$provinsi['nama']}}</option>
+                            @endforeach
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Kab/Kota : </td>
+                    <td id="inputKabkot">
+                        <select name="kabkot" class='form-control'>
+                            <option>select kabupaten / kota</option>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Kecamatan : </td>
+                    <td>
+                        <select name="kec" class='form-control'>
+                            <option>select kecamatan</option>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Nomor Handphone : </td>
+                    <td><input type="number" class='form-control' name="no_hp" value="{{old('no_hp',$DataWarung->no_hp)}}"></td>
+                </tr>
+                <tr>
+                    <td>Koordinat : </td>
+                    <td><input type="text" name="koordinat" value="{{old('koordinat',$DataWarung->koordinat)}}"></td>
+                </tr>
+                <tr>
+                    <td>Jenis Warung : </td>
+                    <td>
+                        <select name="jenis" id="">
+                            <option value="Warung Sembako">Warung Sembako</option>
+                            <option value="Warung Kelontong">Warung Kelontong</option>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Foto Warung : </td>
+                    <td><input type="file" name="foto"></td>
+                </tr>
+            </table>
+            <button type="submit" class="btn btn-success" name="submitedit">simpan & update</button>
+        </form> 
+    </div>
+</div>
 @endsection
 
 @push('scripts')
+    <script src="{{asset('vendor/leaflet/leaflet.js')}}"></script>
     <script>
         $('select[name="prov"]').on('change', function() {
             $.ajax({
@@ -82,6 +90,7 @@
                     $.each(JSON.parse(data), function(i, item) {
                         $('select[name="kabkot"]').append("<option value="+item.id+">"+item.nama+"</option>");
                     });
+                    $('select[name="kec"]').append("<option>select kecamatan</option>");
                 },
                 error : function(jqXHR, textStatus, errorThrown) {
                 },
--- resources/views/warung/index.blade.php
+++ resources/views/warung/index.blade.php
@@ -5,9 +5,20 @@
     </div>
     @foreach ($getMyWarung as $warung)
         <div class="card">
-            {{$warung}}
-            <a href="{{route('warung.view',$warung->id)}}" class="btn btn-info">view</a>
-            <a href="{{route('user.warung.manage',$warung->id)}}" class="btn btn-outline-info">manage</a>
+            <div class="card-body d-flex justify-content-between">
+                <div class="">
+                    <img src="{{asset('img/shop.png')}}" width="100px">
+                    <div class="">
+                        <strong class="proxi">{{$warung->nama_warung}}</strong>
+                        {{$warung->kategori->kategori}}
+                        {{$warung->is_active}}
+                    </div>
+                </div>
+                <div class="d-flex flex-column">
+                    <a href="{{route('warung.view',$warung->id)}}" class="btn btn-info">view</a>
+                    <a href="{{route('user.warung.manage',$warung->id)}}" class="btn btn-outline-info">manage</a>
+                </div>
+            </div>
         </div>
     @endforeach
 @endsection
