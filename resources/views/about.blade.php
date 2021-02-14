e16b612db1ffabd95de8c19586e8464684e82e6c
Rizki Dwi N <72845868+RizkiDwiN@users.noreply.github.com>
2021-02-14 21:20:37 +0700

    add about view

---
+++ resources/views/about.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.main-layout')
+
+@section('content')
+<div class="container-fluid" style="margin-top: 50px;">
+	<br>
+	<center>
+		<h2>Developer Profile</h2>
+		<hr>
+		<table>
+			<tr>
+				<td><img src="{{asset('img/logo.png')}}" style="width:150px; height:150px;"></td>
+				<td>&nbsp;&nbsp;&nbsp;&nbsp;</td>
+				<td><b><br>NIM : 10119043<br><br>Nama : Fermi Naufal Akbar<br><br>Kelas : IF-2</b></td>
+				<td>&nbsp;&nbsp;&nbsp;&nbsp;</td>
+				<td>&nbsp;&nbsp;&nbsp;&nbsp;</td>
+				<td><img src="{{asset('img/logo.png')}}" style="width:150px; height:150px;"></td>
+				<td>&nbsp;&nbsp;&nbsp;&nbsp;</td>
+				<td><b><br>NIM : 10119052<br><br>Nama : Rizki Dwi Nugraha<br><br>Kelas : IF-2</b></td>
+				<td>&nbsp;&nbsp;&nbsp;&nbsp;</td>
+				<td>&nbsp;&nbsp;&nbsp;&nbsp;</td>
+				<td><img src="{{asset('img/logo.png')}}" style="width:150px; height:150px;"></td>
+				<td>&nbsp;&nbsp;&nbsp;&nbsp;</td>
+				<td><b><br>NIM : 10119065<br><br>Nama : Fachriansyah Muh Nur I<br><br>Kelas : IF-2</b></td>
+			</tr>
+
+		</table>
+	</center>
+</div>
+@endsection
+
